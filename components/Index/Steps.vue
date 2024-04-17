2095e70501b60089e08ca60681bbc2bfa0ac6bed
Alain Brenzikofer <alain@integritee.network>
2024-04-17 14:23:52 +0200

    restored design

--- components/Index/Steps.vue
+++ components/Index/Steps.vue
@@ -1,37 +1,81 @@
 <template>
-  <div>
-    <UTabs :items="tabs" v-model:selected="currentTabIndex" class="w-full">
-      <template #default="{ item, selected }">
-        <div @click="changeTab(item.index)" :class="{ 'bg-gray-200': selected }" class="cursor-pointer px-4 py-2">{{ item.label }}</div>
-      </template>
-    </UTabs>
-
-    <component :is="currentTabComponent" @change-tab="changeTab"/>
-
+  <div class="container flex flex-col">
+    <div class=''>
+      <h2 class="title title_h2 partner__title text-left">
+        Campaign
+      </h2>
+    </div>
+    <div class=''>
+      <UTabs :items="items" class="w-full">
+        <template #default="{ item, index, selected }">
+          <div
+              class=" relative truncate"
+              @click="currentTabIndex = index"
+          >
+            <span class="truncate">{{ index + 1 }}. {{ item.label }}</span>
+            <span v-if="selected" class="absolute -right-4 w-2 h-2 rounded-full bg-primary-500 dark:bg-primary-400"/>
+          </div>
+        </template>
+      </UTabs>
+    </div>
+    <div class="tab-content mt-4">
+      <component :is="items[currentTabIndex].component" @change-tab="(i) => currentTabIndex = i"/>
+    </div>
   </div>
 </template>
 
-<script setup>
+
+<script>
 import CheckWalletTab from '~/components/CheckWalletTab.vue'
 import CreateWalletTab from '~/components/CreateWalletTab.vue'
 import InviteFriendTab from '~/components/InviteFriendTab.vue'
 import PublishonxTab from '~/components/PublishonxTab.vue'
 import UnshieldTab from '~/components/UnshieldTab.vue'
-import { ref, computed } from 'vue';
 
-const currentTabIndex = ref(0);
-const tabs = [
-  { label: 'Create Wallet', component: CreateWalletTab, index: 0 },
-  { label: 'Check Balance', component: CheckWalletTab, index: 1 },
-  // Add more tabs as needed
-];
-
-const currentTabComponent = computed(() => tabs[currentTabIndex.value].component);
-
-const changeTab = (index) => {
-  console.log("changeTab called")
-  currentTabIndex.value = index;
-};
+export default {
+  components: {
+    CreateWalletTab,
+    CheckWalletTab,
+    InviteFriendTab,
+    UnshieldTab,
+    PublishonxTab
+  },
+  data() {
+    return {
+      currentTabIndex: 0,
+      items: [
+        {
+          label: 'Create',
+          component: CreateWalletTab
+        },
+        {
+          label: 'Shield',
+          component: CheckWalletTab
+        },
+        {
+          label: 'Invite',
+          component: InviteFriendTab
+        },
+        {
+          label: 'Unshield',
+          component: UnshieldTab
+        },
+        {
+          label: 'Share',
+          component: PublishonxTab
+        }
+      ]
+    }
+  },
+  methods: {
+    onTabChanged(tabIndex) {
+      this.currentComponent = this.items[tabIndex].component;
+    }
+  },
+  mounted() {
+    this.currentComponent = this.items[0].component;
+  }
+}
 </script>
 
 <style lang="scss">

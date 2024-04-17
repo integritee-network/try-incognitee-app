bb4e136be1eac05acd65ed7436e3b20d3f07b8fe
Alain Brenzikofer <alain@integritee.network>
2024-04-17 11:35:44 +0200

    restoring account from url works

--- components/CreateWalletTab.vue
+++ components/CreateWalletTab.vue
@@ -19,7 +19,11 @@ import {u8aToHex} from "@polkadot/util";
 import { ref } from 'vue';
 import type {KeyringPair} from "@polkadot/keyring/types";
 import { useAccount } from '@/store/account.ts'
+import { useRouter } from 'vue-router';
+import {hexToU8a} from '@polkadot/util';
+import { onMounted } from 'vue';
 
+const router = useRouter();
 const accountStore = useAccount()
 
 const currentAccount = ref<KeyringPair | null>(null);
@@ -51,9 +55,16 @@ const createWallet = () => {
   console.log('Updated URL:', window.location.href);
 };
 
-const getSeedFromURI = () => {
-  const searchParams = new URLSearchParams(window.location.search);
-  return searchParams.get('seed');
-};
+onMounted(() => {
+  const seedHex = router.currentRoute.value.query.seed
+  if (seedHex) {
+    console.log("found seed in url: " + seedHex)
+    const localKeyring = new Keyring({ type: 'sr25519' });
+    const account = localKeyring.addFromSeed(hexToU8a(seedHex));
+    console.log(`Account address: ${account.address}`);
+    accountStore.setAccount(account)
+  }
+
+})
 
 </script>

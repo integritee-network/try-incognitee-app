10654aa1c3d3acab824efadc54a7d718cbb06f7e
Alain Brenzikofer <alain@integritee.network>
2024-04-20 10:38:09 +0200

    suggest tweet content by referral link

--- components/PublishonxTab.vue
+++ components/PublishonxTab.vue
@@ -4,19 +4,32 @@
       <div class="container">
         <div class='grid grid-rows-3 grid-flow-col gap-4'>
           <div class='text-4xl mt-10'>Publish on X</div>
-          <div class='text-lg'>By clicking the ”Check” button, you will be redirected to the Rococo Subscan Portal,
-            where you have to check for your unshielding event. Take a screenshot of this event and post it on X
-            including the hashtag #incognitee together with a nice comment. ​
+          <div class='text-lg'>
+            By clicking the ”Check” button, you will be redirected to the Paseo Subscan Portal,
+            where you have to check for your unshielding event.
+            <div class='mt-5'>
+              <template v-if="!isIndexerLinkDisabled">
+                <NuxtLink :to="indexerLinkUrl" target="blank" class="btn btn_gradient">Check Public History</NuxtLink>
+              </template>
+              <template v-else>
+                <NuxtLink to="" target="blank" class="btn btn_border">loading...</NuxtLink>
+              </template>
+            </div>
           </div>
-          <div class='mt-5'>
+          <div class='text-lg'>
 
-            <!-- Use a conditional rendering to show/hide the link based on the disabled state -->
-            <template v-if="!isLinkDisabled">
-              <NuxtLink :to="linkUrl" target="blank" class="btn btn_gradient">Check</NuxtLink>
-            </template>
-            <template v-else>
-              <NuxtLink to="" target="blank" class="btn btn_border">loading...</NuxtLink>
-            </template>
+            To be eligible for our prices, please tweet about our successful test using the button below. Make sure to
+            include hashtag #incognitee and your account
+            <div class='mt-5'>
+              <template v-if="!isTwitterLinkDisabled">
+                <NuxtLink :to="twitterLinkUrl" target="blank" class="btn btn_gradient">tweet about your success
+                </NuxtLink>
+              </template>
+              <template v-else>
+                <NuxtLink to="" target="blank" class="btn btn_border">loading...</NuxtLink>
+              </template>
+            </div>
+
           </div>
         </div>
       </div>
@@ -25,24 +38,35 @@
 </template>
 
 <script setup lang="ts">
-import {ref, watch} from 'vue';
+import {ref, onMounted, watch} from 'vue';
 import {useAccount} from '@/store/account.ts'
 
 const accountStore = useAccount()
 
-const linkUrl = ref("https://paseo.subscan.io");
-const isLinkDisabled = ref(true);
+const indexerLinkUrl = ref("");
+const twitterLinkUrl = ref("");
+const isIndexerLinkDisabled = ref(true);
+const isTwitterLinkDisabled = ref(true);
 
+const updateLinks = () => {
+  if (accountStore.account) {
+    const url = `https://paseo.subscan.io/account/${accountStore.getAddress}?tab=transfer`
+    console.log("setting publish link to " + url)
+    indexerLinkUrl.value = url
+    isIndexerLinkDisabled.value = false
+
+    const incogniteeUrl = encodeURIComponent("https://incognitee.io")
+    const text = encodeURIComponent(`I have successfully tested #incognitee private token transfers with my account ${accountStore.getAddress}`)
+    // todo! upload a tweet image using twitter API
+    twitterLinkUrl.value = `https://twitter.com/intent/tweet?url=${incogniteeUrl}&text=${text}`
+    isTwitterLinkDisabled.value = false
+  }
+}
 watch(
     accountStore,
-    () => {
-      if (accountStore.account) {
-        const url = `https://paseo.subscan.io/account/${accountStore.getAddress}?tab=transfer`
-        console.log("setting publish link to " + url)
-        linkUrl.value = url
-        isLinkDisabled.value = false
-      }
-    },
+    updateLinks,
 )
+
+onMounted(updateLinks)
 
 </script>

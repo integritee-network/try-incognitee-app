8f52389b4d88c2f4e7e327e28f907149304453ce
Christian Langenbacher <clangenb+gh@protonmail.ch>
2024-04-30 15:51:23 +0200

    polyfills are correctly resolved now, but some crypto polyfill is unimplemented

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,6 @@
 import { defineNuxtConfig } from 'nuxt/config'
 
-// Add polyfills: https://github.com/nuxt/nuxt/pull/25028
-import { Buffer } from 'node:buffer'
-import process from 'node:process'
+import 'node:crypto';
 
 export default defineNuxtConfig({
   app: {
@@ -61,7 +59,7 @@ export default defineNuxtConfig({
     externalVue: false,
   },
   routeRules: {
-    '/': { ssr: false },
+    '/': { ssr: true },
   },
   imports: {
     autoImport: false,
@@ -73,7 +71,32 @@ export default defineNuxtConfig({
     defaultImport: 'component',
     global: false,
   },
+  ssr: true,
   vite: {
+    define: {
+      process: {},
+      global: 'globalThis',
+    },
+    // This is not ideal and should work via `unenv` directly..
+    resolve: {
+      alias: {
+        crypto: './node_modules/unenv/runtime/node/crypto/node.cjs',
+        // assert: './node_modules/unenv/runtime/mock/proxy.cjs',
+        http: './node_modules/unenv/runtime/node/http/index.cjs',
+        buffer: './node_modules/unenv/runtime/node/buffer/index.cjs',
+        os: './node_modules/unenv/runtime/mock/proxy.cjs',
+        url: './node_modules/unenv/runtime/node/url/index.cjs',
+        zlib: './node_modules/unenv/runtime/mock/proxy.cjs',
+        'process/': './node_modules/unenv/runtime/mock/proxy.cjs',
+        process: './node_modules/unenv/runtime/mock/proxy.cjs',
+        stream: './node_modules/unenv/runtime/mock/proxy.cjs',
+        _stream_duplex: './node_modules/unenv/runtime/mock/proxy.cjs',
+        _stream_passthrough: './node_modules/unenv/runtime/mock/proxy.cjs',
+        _stream_readable: './node_modules/unenv/runtime/mock/proxy.cjs',
+        _stream_writable: './node_modules/unenv/runtime/mock/proxy.cjs',
+        _stream_transform: './node_modules/unenv/runtime/mock/proxy.cjs',
+      },
+    },
     css: {
       preprocessorOptions: {
         scss: {

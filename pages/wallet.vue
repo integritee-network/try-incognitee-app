40b506e440f6fb49c060973ff10832d1438353bc
Alain Brenzikofer <alain@integritee.network>
2024-06-07 17:41:37 +0200

    prettier

--- pages/wallet.vue
+++ pages/wallet.vue
@@ -862,7 +862,8 @@
                             0.1
                           "
                           required
-                          class="w-full rounded-md border-0 py-1.5 text-black shadow-sm ring-1 ring-inset ring-gray-600 placeholder:text-gray-400 sm:text-sm sm:leading-6" placeholder="Amount"
+                          class="w-full rounded-md border-0 py-1.5 text-black shadow-sm ring-1 ring-inset ring-gray-600 placeholder:text-gray-400 sm:text-sm sm:leading-6"
+                          placeholder="Amount"
                         />
                       </div>
 

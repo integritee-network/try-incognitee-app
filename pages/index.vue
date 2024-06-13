d5721c06a03bc2a978a51dbbc97372c35387c671
alextantumpay <32063118+alex-byteport@users.noreply.github.com>
2024-06-13 11:16:56 +0200

    notification design added

--- pages/index.vue
+++ pages/index.vue
@@ -1415,44 +1415,64 @@
       </Dialog>
     </TransitionRoot>
 
-    <div v-if="showStatusOverlay" class="status-overlay">
-      <div class="status">
-        <div class="spinner" />
-        <div>{{ txStatus }}</div>
-        <button @click="closeStatusOverlay" class="mt-8">close</button>
-      </div>
+    <div aria-live="assertive" class="pointer-events-none fixed inset-0 flex items-end px-4 py-6 sm:items-start sm:p-6">
+    <div class="flex w-full flex-col items-center space-y-4 sm:items-end">
+      <!-- Notification panel, dynamically insert this into the live region when it needs to be displayed -->
+      <transition enter-active-class="transform ease-out duration-300 transition" enter-from-class="translate-y-2 opacity-0 sm:translate-y-0 sm:translate-x-2" enter-to-class="translate-y-0 opacity-100 sm:translate-x-0" leave-active-class="transition ease-in duration-100" leave-from-class="opacity-100" leave-to-class="opacity-0">
+        <div v-if="showStatusOverlay" class="pointer-events-auto w-full max-w-sm overflow-hidden rounded-lg bg-white shadow-lg ring-1 ring-black ring-opacity-5">
+          <div class="p-4">
+            <div class="flex items-center">
+              <div class="flex w-0 flex-1 justify-between">
+                <p class="w-0 flex-1 text-sm font-medium text-gray-900">{{ txStatus }}</p>
+              </div>
+              <div class="ml-4 flex flex-shrink-0">
+                <button type="button" @click="showStatusOverlay = false" class="inline-flex rounded-md bg-white text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+                  <span class="sr-only">Close</span>
+                  <XMarkIcon class="h-5 w-5" aria-hidden="true" />
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </transition>
     </div>
   </div>
+  </div>
+
+  
+
 </template>
 
 <script setup lang="ts">
-import Paseo from "@/assets/img/paseo-logo.svg";
-import Polkadot from "@/assets/img/polkadot-logo.svg";
-import USDC from "@/assets/img/usdc-logo.svg";
+import { XMarkIcon } from '@heroicons/vue/20/solid'
 
-import { useAccount } from "@/store/account.ts";
-import { useIncognitee } from "@/store/incognitee.ts";
+import Paseo from "@/assets/img/paseo-logo.svg"
+import Polkadot from "@/assets/img/polkadot-logo.svg"
+import USDC from "@/assets/img/usdc-logo.svg"
+
+import { useAccount } from "@/store/account.ts"
+import { useIncognitee } from "@/store/incognitee.ts"
 import {
   Dialog,
   DialogPanel,
   DialogTitle,
   TransitionChild,
   TransitionRoot,
-} from "@headlessui/vue";
-import { CheckIcon } from "@heroicons/vue/24/outline";
-import { ApiPromise, WsProvider } from "@polkadot/api";
-import { Keyring } from "@polkadot/keyring";
-import { formatBalance, hexToU8a, u8aToHex } from "@polkadot/util";
+} from "@headlessui/vue"
+import { CheckIcon } from "@heroicons/vue/24/outline"
+import { ApiPromise, WsProvider } from "@polkadot/api"
+import { Keyring } from "@polkadot/keyring"
+import { formatBalance, hexToU8a, u8aToHex } from "@polkadot/util"
 import {
   cryptoWaitReady,
   mnemonicGenerate,
   mnemonicToMiniSecret,
-} from "@polkadot/util-crypto";
-import { useInterval } from "@vueuse/core";
-import { onMounted, ref, watch } from "vue";
-import Qrcode from "vue-qrcode";
-import { QrcodeStream } from "vue-qrcode-reader";
-import { useRouter } from "vue-router";
+} from "@polkadot/util-crypto"
+import { useInterval } from "@vueuse/core"
+import { onMounted, ref, watch } from "vue"
+import Qrcode from "vue-qrcode"
+import { QrcodeStream } from "vue-qrcode-reader"
+import { useRouter } from "vue-router"
 
 const router = useRouter();
 const accountStore = useAccount();

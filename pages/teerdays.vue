068dae64b21e0497c9f402c75619d6e5813ebc3d
Alain Brenzikofer <alain@integritee.network>
2024-08-14 08:25:16 +0200

    add teerdays page

---
+++ pages/teerdays.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="mt-3 text-center sm:mt-5">
+    <span as="h3" class="text-base font-semibold leading-6 text-white"
+      >TEERdays
+    </span>
+
+    <div class="mt-2">
+      <p class="text-sm text-gray-400 text-justify">
+        blabla
+      </p>
+      <!-- this is necessary to avoid the footer overlapping the text -->
+      <br /><br /><br /><br /><br /><br /><br />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+// Import your components and add your setup logic here
+</script>
+
+<style scoped>
+h1 {
+  font-size: 2em; /* Adjust as needed */
+  font-weight: bold; /* Makes the text bold */
+  color: #999; /* Change color as needed */
+  text-align: center; /* Centers the text */
+  margin-bottom: 20px; /* Adds space below the heading */
+}
+
+p {
+  padding: 10px 20px; /* Adds 10px padding top and bottom, 20px padding left and right */
+  line-height: 1.5; /* Increases the space between lines of text */
+  color: #fff; /* Change color as needed */
+  text-align: justify;
+}
+a {
+  color: #99f; /* Change color as needed */
+}
+</style>

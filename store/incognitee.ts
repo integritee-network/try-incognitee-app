dd8a6d02472e8fae81cb897ec35218535efdf414
brenzi <brenzi@users.noreply.github.com>
2024-10-11 08:50:21 +0200

    update worker api and dynamically load fingerprint (#63)

--- store/incognitee.ts
+++ store/incognitee.ts
@@ -1,7 +1,8 @@
 import { defineStore } from "pinia";
 import { IntegriteeWorker } from "@encointer/worker-api";
 import { encodeAddress } from "@polkadot/keyring";
-
+import bs58 from "bs58";
+import { hexToU8a } from "@polkadot/util";
 export const useIncognitee = defineStore("incognitee", {
   state: () => ({
     api: <IntegriteeWorker | null>null,
@@ -12,21 +13,23 @@ export const useIncognitee = defineStore("incognitee", {
   }),
   actions: {
     async initializeApi() {
-      const worker = new IntegriteeWorker(
-        "wss://scv1.paseo.api.incognitee.io:443",
-        {
-          createWebSocket: (url) => new WebSocket(url),
-          types: {},
-        },
-      );
-      this.api = worker;
-      worker.getShardVault().then((sk) => {
-        this.vault = encodeAddress(sk[0]);
-        console.log("Vault: " + this.vault);
+      const url = "wss://scv1.paseo.api.incognitee.io:443";
+      const worker = new IntegriteeWorker(url, {
+        createWebSocket: (url) => new WebSocket(url),
+        types: {},
       });
-      // todo! hard-coded for now. soon to be fetched
+      this.api = worker;
+      // shard is ok to be hard-coded, because we don't want to accidentally change shards
       this.shard = "5wePd1LYa5M49ghwgZXs55cepKbJKhj5xfzQGfPeMS7c";
-      this.fingerprint = "DfA6i67tKRZS82Uv5W9zhsFdouZizUioB7zLbiSktRDK";
+      const sk = await worker.getShardVault();
+      this.vault = encodeAddress(sk[0]);
+      console.log("Vault: " + this.vault);
+      const fingerprint_hex = await worker.getFingerprint();
+      this.fingerprint = bs58.encode(hexToU8a(fingerprint_hex.toString()));
+      console.log(
+        `validateer at ${url} reported fingerprint: ` + this.fingerprint,
+      );
+      //todo: verify fingerprint against teerex
       console.log("Incognitee Api connected to sidechain");
       this.apiReady = true;
     },

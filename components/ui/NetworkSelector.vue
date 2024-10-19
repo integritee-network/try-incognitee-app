73899b57d86946eaa9c6c3476d34ee52b3f864ce
brenzi <brenzi@users.noreply.github.com>
2024-10-19 17:38:22 +0200

    modularize and declutter (#69)
    * shielding Overlay modularized. works
    
    * all actions modularized
    
    * fix privacy info too
    
    * finished decluttering

---
+++ components/ui/NetworkSelector.vue
@@ -0,0 +1,45 @@
+<template>
+  <div
+    class="flex flex-row justify-between w-full text-center gap-4 container mb-10"
+  >
+    <div class="currency-box p-3 gap-2 basis-1/2" @click="openAssetsInfo">
+      <Polkadot class="w-[30px] h-[30px]" />
+      <p class="text-xs">DOT</p>
+      <span
+        class="inline-flex items-center rounded-lg bg-gray-100 px-1.5 py-0.5 text-[10px] font-medium text-gray-600"
+        >coming soon</span
+      >
+    </div>
+    <div
+      class="currency-box gap-2 p-3 basis-1/2 border-2 border-incognitee-green"
+    >
+      <Paseo class="w-[30px] h-[30px]" />
+      <p class="text-xs">PAS<br />PASEO</p>
+    </div>
+    <div class="currency-box gap-2 p-3 basis-1/2" @click="openAssetsInfo">
+      <USDC class="w-[30px] h-[30px]" />
+      <p class="text-xs">USDC</p>
+      <span
+        class="inline-flex items-center rounded-lg bg-gray-100 px-1.5 py-0.5 text-[10px] font-medium text-gray-600"
+        >coming soon</span
+      >
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import Polkadot from "assets/img/polkadot-logo.svg";
+import Paseo from "assets/img/paseo-logo.svg";
+import USDC from "assets/img/usdc-logo.svg";
+
+import { defineProps } from "vue";
+
+const props = defineProps({
+  openAssetsInfo: {
+    type: Function,
+    required: true,
+  },
+});
+</script>
+
+<style scoped></style>
---
+++ components/ui/OverlayDialog.vue
@@ -0,0 +1,85 @@
+<template>
+  <TransitionRoot as="template" :show="show">
+    <Dialog class="relative z-20" @close="close">
+      <TransitionChildSootGlass />
+      <div class="fixed inset-0 z-10 w-screen overflow-y-auto">
+        <div
+          class="flex items-center justify-center p-4 text-center sm:items-center sm:p-0"
+        >
+          <TransitionChild
+            as="template"
+            enter="ease-out duration-300"
+            enter-from="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+            enter-to="opacity-100 translate-y-0 sm:scale-100"
+            leave="ease-in duration-200"
+            leave-from="opacity-100 translate-y-0 sm:scale-100"
+            leave-to="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+          >
+            <DialogPanel
+              class="w-full relative transform overflow-hidden rounded-lg bg-gray-800 px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-sm sm:p-6"
+            >
+              <div class="absolute right-0 top-0 pr-4 pt-4">
+                <button
+                  type="button"
+                  class="text-gray-400 hover:text-gray-500"
+                  @click="close"
+                >
+                  <span class="sr-only">Close</span>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    class="h-6 w-6"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    stroke-width="2"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M6 18L18 6M6 6l12 12"
+                    />
+                  </svg>
+                </button>
+              </div>
+              <div class="mt-3 text-center sm:mt-5 flex-grow">
+                <DialogTitle
+                  as="h3"
+                  class="text-base font-semibold leading-6 text-white"
+                  >{{ title }}
+                </DialogTitle>
+                <slot />
+              </div>
+            </DialogPanel>
+          </TransitionChild>
+        </div>
+      </div>
+    </Dialog>
+  </TransitionRoot>
+</template>
+
+<script setup lang="ts">
+import {
+  Dialog,
+  DialogPanel,
+  DialogTitle,
+  TransitionChild,
+  TransitionRoot,
+} from "@headlessui/vue";
+import { defineProps } from "vue";
+import TransitionChildSootGlass from "@/components/ui/TransitionChildSootGlass.vue";
+
+const props = defineProps({
+  close: {
+    type: Function,
+    required: true,
+  },
+  show: {
+    type: Boolean,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+});
+</script>

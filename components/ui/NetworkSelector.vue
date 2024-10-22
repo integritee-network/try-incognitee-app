45fbe6fefd9cda3b0825855c26eb9a6777c1180d
Alain Brenzikofer <alain@integritee.network>
2024-10-22 10:59:35 +0200

    add beta banner to TEER

--- components/ui/NetworkSelector.vue
+++ components/ui/NetworkSelector.vue
@@ -11,13 +11,17 @@
       <p class="text-xs">TEER<br />Integritee</p>
     </a>
     -->
-    <div class="currency-box gap-2 p-3 basis-1/3" @click="openAssetsInfo">
+    <div
+      class="currency-box gap-2 p-3 basis-1/3 relative"
+      @click="openAssetsInfo"
+    >
       <TEER class="w-[30px] h-[30px]" />
       <p class="text-xs">TEER</p>
       <span
         class="inline-flex items-center rounded-lg bg-gray-100 px-1.5 py-0.5 text-[10px] font-medium text-gray-600"
         >coming soon</span
       >
+      <div class="ribbon"><span>beta</span></div>
     </div>
     <div
       class="currency-box gap-2 p-3 basis-1/3 border-2 border-incognitee-green"
@@ -51,4 +55,29 @@ defineProps({
 });
 </script>
 
-<style scoped></style>
+<style scoped>
+.ribbon {
+  position: absolute;
+  top: 0;
+  right: 0;
+  width: 75px;
+  height: 75px;
+  overflow: hidden;
+  pointer-events: none;
+}
+
+.ribbon span {
+  position: absolute;
+  display: block;
+  width: 100px;
+  padding: 0px 0;
+  background-color: red;
+  color: white;
+  font-size: 12px;
+  font-weight: bold;
+  text-align: center;
+  transform: rotate(45deg);
+  top: 9px;
+  right: -33px;
+}
+</style>

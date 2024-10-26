b186603f4ace093c0ebb6d13c4edb99b3f9e0494
Alain Brenzikofer <alain@integritee.network>
2024-10-26 20:30:28 +0200

    basig health for teerdays

--- components/ui/HealthDetailsOverlay.vue
+++ components/ui/HealthDetailsOverlay.vue
@@ -2,13 +2,38 @@
   <OverlayDialog :show="show" :close="close" title="System Health">
     <div class="mt-2">
       <p class="text-sm text-gray-400">blabla</p>
+      <p class="text-sm text-gray-400">
+        Integritee Network Block Height:
+        {{
+          formatBlockNumber(
+            systemHealth.getIntergiteeBlockNumberObservable?.value,
+          )
+        }}
+        {{ integriteeNetworkBlockNumberAge }}s
+      </p>
     </div>
   </OverlayDialog>
 </template>
 
 <script setup lang="ts">
 import OverlayDialog from "@/components/ui/OverlayDialog.vue";
-import { defineProps } from "vue";
+import { defineProps, ref, watch } from "vue";
+import { useSystemHealth } from "@/store/systemHealth";
+import { useInterval } from "@vueuse/core";
+
+const systemHealth = useSystemHealth();
+
+const integriteeNetworkBlockNumberAge = ref(0);
+
+const tickerCounter = useInterval(300);
+watch(tickerCounter, () => {
+  const observable = systemHealth.getIntergiteeBlockNumberObservable;
+  if (observable?.timestamp) {
+    integriteeNetworkBlockNumberAge.value = Math.round(
+      (Date.now() - observable?.timestamp.getTime()) / 1000,
+    );
+  }
+});
 
 const props = defineProps({
   show: {
@@ -20,6 +45,14 @@ const props = defineProps({
     required: true,
   },
 });
+
+const formatBlockNumber = (blockNumber: number) => {
+  return blockNumber.toLocaleString("de-CH", {
+    minimumFractionDigits: 0,
+    maximumFractionDigits: 0,
+    thousandsSeparator: "'",
+  });
+};
 </script>
 
 <style scoped></style>
--- store/systemHealth.ts
+++ store/systemHealth.ts
@@ -4,12 +4,25 @@ import { ChainId } from "@/configs/chains";
 class ObservableNumber {
   value: number;
   timestamp: Date;
+  lastDuration: number;
+  lastValue: number;
   constructor(value: number) {
     this.value = value;
+    this.timestamp = new Date();
   }
   observe(new_value: number) {
+    if (new_value === this.value) {
+      return;
+    }
+    this.lastDuration = this.age();
+    this.lastValue = this.value;
     this.value = new_value;
     this.timestamp = new Date();
+  }
+
+  // milliseconds
+  age() {
+    return new Date().getTime() - this.timestamp.getTime();
   }
 }
 
@@ -23,18 +36,33 @@ export enum Health {
 export const useSystemHealth = defineStore("system-health", {
   state: () => ({
     shieldingTargetFinalizedBlockNumber: <ObservableNumber | null>null,
+    integriteeLastBlockNumber: <ObservableNumber | null>null,
   }),
   getters: {
     getAggregatedSystemHealth({ shieldingTargetFinalizedBlockNumber }): Health {
       return Health.Healthy;
     },
     getIntegriteeSystemHealth({ shieldingTargetFinalizedBlockNumber }): Health {
-      return Health.Warning;
+      let health = Health.Healthy;
+      if (this.integriteeLastBlockNumber?.age() > 24000) {
+        health = Health.Warning;
+      }
+      return health;
+    },
+    getIntergiteeBlockNumberObservable({
+      integriteeLastBlockNumber,
+    }): ObservableNumber {
+      return integriteeLastBlockNumber;
     },
   },
   actions: {
-    setShieldingTargetFinalizedBlockNumber(block_number: number) {
-      this.shieldingTargetFinalizedBlockNumber.observe(block_number);
+    observeShieldingTargetFinalizedBlockNumber(block_number: number) {
+      this.shieldingTargetFinalizedBlockNumber?.observe(block_number);
+    },
+    observeIntegriteeBlockNumber(block_number: number) {
+      this.integriteeLastBlockNumber
+        ? this.integriteeLastBlockNumber?.observe(block_number)
+        : (this.integriteeLastBlockNumber = new ObservableNumber(block_number));
     },
   },
 });

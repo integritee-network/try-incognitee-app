e8c4ba672afddc7aa66c58d2396db5e9232f33df
Alain Brenzikofer <alain@integritee.network>
2025-03-12 10:46:36 +0100

    fix #111

--- configs/chains.ts
+++ configs/chains.ts
@@ -23,23 +23,25 @@ type ChainConfig = {
 export const chainConfigs: Record<ChainId, ChainConfig> = {
   [ChainId.PolkadotRelay]: {
     name: "Polkadot Relay",
-    api: "wss://rpc.polkadot.io",
+    api: ["wss://rpc.ibp.network/polkadot", "wss://rpc.polkadot.io"],
   },
   [ChainId.KusamaRelay]: {
     name: "Kusama Relay",
-    api: "wss://kusama-rpc.polkadot.io",
+    api: ["wss://rpc.ibp.network/kusama", "wss://kusama-rpc.polkadot.io"],
   },
   [ChainId.PaseoRelay]: {
     name: "Paseo Relay",
-    api: "wss://paseo.dotters.network",
-    //api: "wss://rpc.ibp.network/paseo",
+    api: ["wss://rpc.ibp.network/paseo", "wss://paseo.dotters.network"],
     faucetUrl: "https://faucet.polkadot.io/",
     //faucetUrl: "https://substratefaucet.xyz/paseo/ADDRESS"
     indexerUrls: ["https://paseo.subscan.io/account/ADDRESS?tab=transfer"],
   },
   [ChainId.AssetHubPolkadot]: {
     name: "Asset Hub Polkadot",
-    api: "wss://polkadot-asset-hub-rpc.polkadot.io",
+    api: [
+      "wss://sys.ibp.network/asset-hub-polkadot",
+      "wss://polkadot-asset-hub-rpc.polkadot.io",
+    ],
     indexerUrls: [
       "https://assethub-polkadot.subscan.io/account/ADDRESS?tab=transfer",
       "https://assethub-polkadot.statescan.io/#/accounts/ADDRESS?tab=transfers",
@@ -47,11 +49,17 @@ export const chainConfigs: Record<ChainId, ChainConfig> = {
   },
   [ChainId.AssetHubKusama]: {
     name: "Asset Hub Kusama",
-    api: "wss://kusama-asset-hub-rpc.polkadot.io",
+    api: [
+      "wss://sys.ibp.network/asset-hub-kusama",
+      "wss://kusama-asset-hub-rpc.polkadot.io",
+    ],
   },
   [ChainId.AssetHubPaseo]: {
     name: "Asset Hub Paseo",
-    api: "wss://sys.ibp.network/asset-hub-paseo",
+    api: [
+      "wss://sys.ibp.network/asset-hub-paseo",
+      "wss://pas-rpc.stakeworld.io/assethub",
+    ],
     faucetUrl: "https://faucet.polkadot.io/?parachain=1000",
     indexerUrls: [
       "https://assethub-paseo.subscan.io/account/ADDRESS?tab=transfer",
@@ -59,11 +67,17 @@ export const chainConfigs: Record<ChainId, ChainConfig> = {
   },
   [ChainId.IntegriteePolkadot]: {
     name: "Integritee Polkadot",
-    api: "wss://polkadot.api.integritee.network",
+    api: [
+      "wss://polkadot.api.integritee.network",
+      "wss://integritee-rpc.dwellir.com",
+    ],
   },
   [ChainId.IntegriteeKusama]: {
     name: "Integritee Kusama",
-    api: "wss://kusama.api.integritee.network",
+    api: [
+      "wss://kusama.api.integritee.network",
+      "wss://integritee-kusama.api.onfinality.io/public-w",
+    ],
     indexerUrls: ["https://integritee.subscan.io/account/ADDRESS?tab=transfer"],
   },
   [ChainId.IntegriteePaseo]: {
